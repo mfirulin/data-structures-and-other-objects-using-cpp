3975886df9896ba8ad21c6db1adee2c73fee71f7
Firulin <mfirulin@merann.ru>
2020-09-28 17:21:22 +0300

    Chapter 1.1. Exercise 5.

--- meters_to_feet.cpp
+++ meters_to_feet.cpp
@@ -1,1 +1,52 @@
 #include <cstdlib>
+#include <cassert>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+// parameter m - meters, should be positive.
+// return - feet
+double meters_to_feet(double m)
+{
+    assert(m >= 0);
+
+    return m * 3.2808;
+}
+
+void setup_cout_fraction(int fraction_digits)
+{
+    assert(fraction_digits > 0);
+
+    cout.precision(fraction_digits);
+    cout.setf(ios::fixed, ios::floatfield);
+    if (fraction_digits > 0)
+        cout.setf(ios::showpoint);
+    else
+        cout.unsetf(ios::showpoint);
+        
+}
+
+int main()
+{
+    const int START = 5;
+    const int END = 100;
+    const int STEP = 5;
+    const int FRACTION = 2;
+    const int WIDTH = 9;
+    const string HEADER1 = "meters";
+    const string HEADER2 = "feet";
+
+    setup_cout_fraction(FRACTION);
+
+    cout << setw(WIDTH) << HEADER1 << ' ';
+    cout << setw(WIDTH) << HEADER2 << endl;
+
+    for (int m = START; m <= END; m += STEP) {
+        double f = meters_to_feet(m);
+        cout << setw(WIDTH) << m << ' ';
+        cout << setw(WIDTH) << f << endl;
+    }
+
+    return EXIT_SUCCESS;
+}
